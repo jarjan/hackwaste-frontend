58cec4d28312fe75ae68dc4155276d9e470403f0
Merey Zholdas <merey.90@gmail.com>
2019-09-07 18:03:08 +0200

    categories

--- store/__mocks__.js
+++ store/__mocks__.js
@@ -1,11 +1,11 @@
-import { ProductStatus } from './types'
+import { ProductStatus, ProductCategry } from './types'
 
 export const mockProduct = {
   id: 1,
   ean: '4260397980354',
   title: 'Lucuma & cacao-nibs',
   dueDate: '7th September 2019', // date on product description
-  category: 'beans',
+  category: ProductCategry.Gewürze,
   expirationDate: '16th September 2019', // date estimated by app, fe: dueDate+12days
   status: ProductStatus.pending,
   imgSrc: 'https://www.fructo.com.ua/image/cache/catalog/fruct/avokato-1400x1400.png',
--- store/types.js
+++ store/types.js
@@ -4,3 +4,26 @@ export const ProductStatus = {
   consumed: 'consumed',
   pending: 'pending',
 }
+
+export const ProductCategry = {
+  'Brot & Backwaren': 'Brot & Backwaren',
+  Cerealien: 'Cerealien',
+  Grundnahrungsmittel: 'Grundnahrungsmittel',
+  Teigwaren: 'Teigwaren',
+  Brotaufstriche: 'Brotaufstriche',
+  'Öl & Essig': 'Öl & Essig',
+  'Soßen/Suppen/Würz-Mischungen': 'Soßen/Suppen/Würz-Mischungen',
+  Gewürze: 'Gewürze',
+  Salzgebäck: 'Salzgebäck',
+  Süßgebäck: 'Süßgebäck',
+  Süßwaren: 'Süßwaren',
+  'Super & Powerfood (Proteinnahrung)': 'Super & Powerfood (Proteinnahrung)',
+  Obstkonserven: 'Obstkonserven',
+  Sauerkonserven: 'Sauerkonserven',
+  'Obst und Gemüse': 'Obst und Gemüse',
+  Getränke: 'Getränke',
+  Heißgetränke: 'Heißgetränke',
+  Spirituosen: 'Spirituosen',
+  Wein: 'Wein',
+  Nonfood: 'Nonfood',
+}
