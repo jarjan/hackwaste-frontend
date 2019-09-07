fb92f24cec56139d1962818bce526f66d718dd52
Olzhas Askar <olzhas.askar@gmail.com>
2019-09-07 19:15:13 +0200

    Add expirations

---
+++ pages/expirations.js
@@ -0,0 +1,73 @@
+import moment from 'moment'
+
+export const calculateWasteDate = (category, bestBeforeDate) => {
+  let wasteDate
+
+  switch (category) {
+    case 'Backwaren​​​':
+      wasteDate = moment(bestBeforeDate).add('1', 'month')
+      break
+    case 'Cerealien​​​​':
+      wasteDate = moment(bestBeforeDate).add('6', 'months')
+      break
+    case 'Fertiggerichte​​​​':
+      wasteDate = moment(bestBeforeDate).add('2', 'months')
+      break
+    case 'Grundnahrungsmittel​​​':
+      wasteDate = moment(bestBeforeDate).add('2', 'months')
+      break
+    case 'Teigwaren​​​​':
+      wasteDate = moment(bestBeforeDate).add('12', 'months')
+      break
+    case 'Brotaufstriche​​​​':
+      wasteDate = moment(bestBeforeDate).add('12', 'months')
+      break
+    case 'Öl':
+      wasteDate = moment(bestBeforeDate).add('1', 'months')
+      break
+    case 'Essig​​​​':
+      wasteDate = moment(bestBeforeDate).add('1', 'months')
+      break
+    case 'Soßen':
+      wasteDate = moment(bestBeforeDate).add('12', 'months')
+      break
+    case 'Suppen':
+      wasteDate = moment(bestBeforeDate).add('12', 'months')
+      break
+    case 'Gewürze​​​​':
+      wasteDate = moment(bestBeforeDate).add('12', 'months')
+      break
+    case 'Salzgebäck​​​​':
+      wasteDate = moment(bestBeforeDate).add('6', 'months')
+      break
+    case 'Süßgebäck​​​​':
+      wasteDate = moment(bestBeforeDate).add('6', 'months')
+      break
+    case 'Süßwaren​​​​':
+      wasteDate = moment(bestBeforeDate).add('2', 'months')
+      break
+    case 'Powerfood':
+      wasteDate = moment(bestBeforeDate).add('2', 'months')
+      break
+    case 'Obstkonserven':
+      wasteDate = moment(bestBeforeDate).add('12', 'months')
+      break
+    case 'Sauerkonserven':
+      wasteDate = moment(bestBeforeDate).add('12', 'months')
+      break
+    case 'Obst':
+      wasteDate = moment(bestBeforeDate).add('2', 'months')
+      break
+    case 'Gemüse​​​':
+      wasteDate = moment(bestBeforeDate).add('2', 'months')
+      break
+    case 'Getränke​​':
+      wasteDate = moment(bestBeforeDate).add('2', 'months')
+      break
+    case 'Heißgetränke':
+      wasteDate = moment(bestBeforeDate).add('12', 'months')
+      break
+  }
+
+  return wasteDate
+}
