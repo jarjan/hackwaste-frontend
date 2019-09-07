de19fee00e0585867e99660157677ec72187aeb1
Zharzhan Kulmyrza <zharzhan@gmail.com>
2019-09-07 15:11:59 +0200

    Cards boilerplate

--- components/Card.vue
+++ components/Card.vue
@@ -1,34 +1,18 @@
 <template>
-  <div class="column">
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title has-text-grey">{{ title }}</p>
-      </header>
-      <div class="card-content">
-        <div class="content has-text-centered">
-          <b-icon :icon="icon" size="is-large" type="is-primary" />
-        </div>
-      </div>
-      <footer class="card-footer">
-        <div class="card-footer-item">
-          <span>
-            <slot />
-          </span>
-        </div>
-      </footer>
-    </div>
+  <div class="card" :class="{ isCurrent: isCurrent }">
+    <h3 class="cardTitle">{{ card.keyword }}</h3>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    title: {
-      type: String,
+    card: {
+      type: Object,
       required: true,
     },
-    icon: {
-      type: String,
+    isCurrent: {
+      type: Boolean,
       required: true,
     },
   },
---
+++ components/CardsStack.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cards">
+    <Card v-for="(card, index) in cards" :key="card" :card="card" :is-current="index === 0" />
+  </div>
+</template>
+
+<script>
+import Card from './Card'
+export default {
+  components: {
+    Card,
+  },
+  props: {
+    cards: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
