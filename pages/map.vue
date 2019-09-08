283ffb2f71167a3e9a0272fdd2e6c0a92a7e805f
Zharzhan Kulmyrza <zharzhan@gmail.com>
2019-09-08 10:55:32 +0200

    working map

--- pages/map.vue
+++ pages/map.vue
@@ -1,24 +1,26 @@
 <template>
   <section class="section">
-    <client-only>
-      <l-map
-        :zoom="zoom"
-        :center="center"
-        :options="mapOptions"
-        style="height: 80%"
-        @update:center="centerUpdate"
-        @update:zoom="zoomUpdate"
-      >
-        <l-tile-layer :url="url" :attribution="attribution" />
-        <l-marker v-for="marker in markers" :key="marker.name" :lat-lng="marker.location">
-          <l-tooltip :options="{ permanent: true, interactive: true }">
-            <div>
-              {{ marker.name }}
-            </div>
-          </l-tooltip>
-        </l-marker>
-      </l-map>
-    </client-only>
+    <div id="map-wrap" style="height: 100vh">
+      <client-only>
+        <l-map
+          :zoom="zoom"
+          :center="center"
+          :options="mapOptions"
+          style="height: 80%"
+          @update:center="centerUpdate"
+          @update:zoom="zoomUpdate"
+        >
+          <l-tile-layer :url="url" :attribution="attribution" />
+          <l-marker v-for="marker in markers" :key="marker.name" :lat-lng="marker.location">
+            <l-tooltip :options="{ permanent: true, interactive: true }">
+              <div>
+                {{ marker.name }}
+              </div>
+            </l-tooltip>
+          </l-marker>
+        </l-map>
+      </client-only>
+    </div>
   </section>
 </template>
 
@@ -30,12 +32,12 @@ export default {
 
   data() {
     return {
-      zoom: 13,
-      center: latLng(47.41322, -1.219482),
+      zoom: 12,
+      center: latLng(52.520008, 13.404954),
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       currentZoom: 11.5,
-      currentCenter: latLng(47.41322, -1.219482),
+      currentCenter: latLng(52.520008, 13.404954),
       showParagraph: false,
       mapOptions: {
         zoomSnap: 0.5,
