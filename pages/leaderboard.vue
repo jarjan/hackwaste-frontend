266b7da6cac23124238da424e870d29540f15457
Merey Zholdas <merey.90@gmail.com>
2019-09-08 11:11:00 +0200

    leaderboard page

---
+++ pages/leaderboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section">
+    <b-table :data="leaders" :columns="columns"></b-table>
+  </section>
+</template>
+
+<script>
+import { mockLeaders } from '~/static/__mocks__'
+
+export default {
+  name: 'ProductsPage',
+
+  data() {
+    return {}
+  },
+  computed: {
+    leaders() {
+      return mockLeaders
+    },
+    person() {
+      const person = {
+        id: Date.now(),
+        name: 'Big brother',
+        score: this.$store.state.game.points,
+        imgSrc: '/profiles/user.png',
+      }
+      return person
+    },
+  },
+}
+</script>
